ab6bba998e6ba3faf83955dfe4982fef02a23e65
T-Wills <estherwilliams195@gmail.com>
2021-07-01 12:34:16 +0100

    App.js file changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from "./component/Home/Home";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   
